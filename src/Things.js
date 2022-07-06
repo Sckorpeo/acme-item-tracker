7b44aab4988a9f183d834c4216a55bae888d649f
Kieran B <kieranbru.web@gmail.com>
2022-07-06 16:47:39 -0400

    Added thing delete functionality

--- src/Things.js
+++ src/Things.js
@@ -1,31 +1,45 @@
 import React from 'react';
 import ThingForm from './ThingForm';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
-const Things = ({ things })=> {
-  return (
-    <div>
-      <h1>Things</h1>
-      <ul>
-        {
-          things.map( thing => {
-            return (
-              <li key={ thing.id }>
-                { thing.name }
-              </li>
-            );
-          })
-        }
-      </ul>
-      <ThingForm />
-    </div>
-  );
+const Things = ({ things, deleteThing }) => {
+	return (
+		<div>
+			<h1>Things</h1>
+			<ul>
+				{
+					things.map(thing => {
+						return (
+							<li key={thing.id}>
+								{thing.name}
+								<button onClick={() => deleteThing(thing.id)}>X</button>
+							</li>
+						);
+					})
+				}
+			</ul>
+			<ThingForm />
+		</div>
+	);
 };
 
-export default connect(
-  (state)=> {
-    return {
-      things: state.things
-    }
-  }
-)(Things);
+const mapStateToProps = (state) => {
+	const obj = {
+		things: state.things
+	}
+	return obj;
+}
+
+const mapDispatchToProps = (dispatch) => {
+	async function deleteThing(id) {
+		await axios.delete(`api/things/${id}`);
+		dispatch({ type: 'DELETE_THING', thingId: id });
+	}
+	const obj = {
+		deleteThing
+	}
+	return obj;
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Things);
--- src/store.js
+++ src/store.js
@@ -1,25 +1,29 @@
 import { createStore } from 'redux';
 
 const initialState = {
-  view: window.location.hash.slice(1),
-  users: [],
-  things: []
+	view: window.location.hash.slice(1),
+	users: [],
+	things: []
 };
 
-const store = createStore((state = initialState, action)=> { 
-  if(action.type === 'SET_THINGS'){
-    return {...state, things: action.things };
-  }
-  if(action.type === 'SET_USERS'){
-    return {...state, users: action.users }; 
-  }
-  if(action.type === 'SET_VIEW'){
-    return {...state, view: action.view }; 
-  }
-  if(action.type === 'CREATE_THING'){
-    return {...state, things: [...state.things, action.thing ]}; 
-  }
-  return state;
+const store = createStore((state = initialState, action) => {
+	if (action.type === 'SET_THINGS') {
+		return { ...state, things: action.things };
+	}
+	if (action.type === 'SET_USERS') {
+		return { ...state, users: action.users };
+	}
+	if (action.type === 'SET_VIEW') {
+		return { ...state, view: action.view };
+	}
+	if (action.type === 'CREATE_THING') {
+		return { ...state, things: [...state.things, action.thing] };
+	}
+	if (action.type === 'DELETE_THING') {
+		const newThings = state.things.filter(item => item.id !== action.thingId);
+		return { ...state, things: newThings };
+	}
+	return state;
 });
 
 export default store;
