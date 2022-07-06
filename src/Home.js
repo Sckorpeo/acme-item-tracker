23dd048cbdaf618bd830575703718d40442d4e58
Kieran B <kieranbru.web@gmail.com>
2022-07-06 17:50:02 -0400

    Added highest ranked thing to home page

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Home = ({ users, things })=> {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>
-        Here at the Acme Item Tracker Corp we have { users.length } users and { things.length } things!
-      </p>
-    </div>
-  );
+const Home = ({ users, things }) => {
+	const highestThing = things.sort((a, b) => b.rank - a.rank)[0] || undefined;
+	return (
+		<div>
+			<h1>Home</h1>
+			<p>
+				Here at the Acme Item Tracker Corp we have {users.length} users and {things.length} things!
+			</p>
+			<div>
+				Current highest ranked thing {highestThing?.name?.toUpperCase()} with a rank of {highestThing?.rank}
+			</div>
+		</div>
+	);
 };
 
-const mapSToP = (s)=> {
-  return {
-    users: s.users,
-    things: s.things
-  };
+const mapSToP = (s) => {
+	return {
+		users: s.users,
+		things: s.things
+	};
 };
 
 export default connect(mapSToP)(Home);
