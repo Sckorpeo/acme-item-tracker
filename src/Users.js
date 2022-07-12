7bd0ff0082c445df8fd4ec51f0b9a1ca0c90c446
Kieran B <kieranbru.web@gmail.com>
2022-07-11 21:30:06 -0400

    Test

--- src/Users.js
+++ src/Users.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React from 'react';
 import { connect } from 'react-redux';
 
+const newVar = null;
 
 const Users = ({ users, deleteUser }) => {
 	return (
--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 
 const initialState = {
 	view: window.location.hash.slice(1),
@@ -6,33 +7,49 @@ const initialState = {
 	things: []
 };
 
-const store = createStore((state = initialState, action) => {
-	if (action.type === 'SET_THINGS') {
-		return { ...state, things: action.things };
-	}
-	if (action.type === 'SET_USERS') {
-		return { ...state, users: action.users };
-	}
+const viewReducer = (state = window.location.hash.slice(1), action) => {
 	if (action.type === 'SET_VIEW') {
-		return { ...state, view: action.view };
+		return action.view;
 	}
-	if (action.type === 'CREATE_THING') {
-		return { ...state, things: [...state.things, action.thing] };
-	}
-	if (action.type === 'DELETE_THING') {
-		const newThings = state.things.filter(item => item.id !== action.thingId);
-		return { ...state, things: newThings };
+	return state;
+};
+
+const usersReducer = (state = [], action) => {
+	if (action.type === 'SET_USERS') {
+		return action.users;
 	}
 	if (action.type === 'DELETE_USER') {
 		const newUsers = state.users.filter(user => user.id !== action.userId);
-		return { ...state, users: newUsers };
+		return newUsers;
+	}
+	return state;
+};
+
+const thingsReducer = (state = [], action) => {
+	if (action.type === 'SET_THINGS') {
+		return action.things;
+	}
+	if (action.type === 'CREATE_THING') {
+		return [...state.things, action.thing];
+	}
+	if (action.type === 'DELETE_THING') {
+		const newThings = state.things.filter(item => item.id !== action.thingId);
+		return newThings;
 	}
 	if (action.type === 'CHANGE_RANK') {
 		const thingArr = state.things.filter(item => item.id !== action.thingId);
 		return { ...state, things: [...thingArr, action.updatedThing] };
 	}
 	return state;
+};
+
+const reducer = combineReducers({
+	users: usersReducer,
+	things: thingsReducer,
+	view: viewReducer
 });
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 export default store;
 
