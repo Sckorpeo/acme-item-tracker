eb583fa7470a644cc6f5216467df12fd75c63e1a
Kieran B <kieranbru.web@gmail.com>
2022-07-11 21:49:37 -0400

    Fix spacing

--- src/Users.js
+++ src/Users.js
@@ -2,45 +2,42 @@ import axios from 'axios';
 import React from 'react';
 import { connect } from 'react-redux';
 
-const newVar = null;
-const anotherOne = null;
-
 const Users = ({ users, deleteUser }) => {
-	return (
-		<div>
-			<h1>Users</h1>
-			<ul>
-				{
-					users.map(user => {
-						return (
-							<li key={user.id}>
-								{user.name}
-								<button onClick={() => deleteUser(user.id)}>X</button>
-							</li>
-						);
-					})
-				}
-			</ul>
-		</div>
-	);
+    return (
+        <div>
+            <h1>Users</h1>
+            <ul>
+                {
+                    users.map(user => {
+                        return (
+                            <li key={user.id}>
+                                {user.name}
+                                <button onClick={() => deleteUser(user.id)}>X</button>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </div>
+    );
 }
 
 const mapDispatchToProps = (dispatch) => {
-	async function deleteUser(id) {
-		await axios.delete(`api/users/${id}`);
-		dispatch({ type: 'DELETE_USER', userId: id });
-	}
-	const obj = {
-		deleteUser
-	}
-	return obj;
+    async function deleteUser(id) {
+        await axios.delete(`api/users/${id}`);
+        dispatch({ type: 'DELETE_USER', userId: id });
+    }
+    const obj = {
+        deleteUser
+    }
+    return obj;
 }
 
 const mapStateToProps = (state) => {
-	const obj = {
-		users: state.users
-	}
-	return obj
+    const obj = {
+        users: state.users
+    }
+    return obj
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Users);
